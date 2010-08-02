da86354dd03b62c40a0c5671e2e308d48d210adf
Adrien Saladin <adrien@saladin.fr>
2010-08-02 16:30:13 +0200

    first try of logging method call

--- spyproxy.py
+++ spyproxy.py
@@ -4,13 +4,28 @@ class SpyProxy(object):
       object.__setattr__(self, "instance", instance)
       object.__setattr__(self, "spyname", spyname)
       object.__setattr__(self, "logfile", logfile)
+      
+      
+   def _logmethod(self,method, methodname):
+      def newmethod(*a,**b):
+	  logfile = object.__getattribute__(self, "logfile")
+          log = "calling callable '%s' with positional arguments %s and named arguments %s\n"%(methodname,str(a),str(b))
+          logfile.write(log)
+          return method(*a,**b)
+      return newmethod
+      
    def __getattribute__(self, attrname):
       spyname = object.__getattribute__(self, "spyname")
       logfile = object.__getattribute__(self, "logfile")
-      log = "in %s (%i): attribute '%s' read\n"%(spyname,id(object.__getattribute__(self, "instance")), attrname)
+      attribute = getattr(object.__getattribute__(self, "instance"), attrname)
+      attributetype = type(attribute)
+      print type(attribute)
+      if callable(attribute):
+          decorator = object.__getattribute__(self, "_logmethod")
+          attribute = decorator(attribute, attrname)
+      log = "in %s (%i): reading attribute '%s' of type %s\n"%(spyname,id(attribute), attrname,str(attributetype))
       logfile.write(log)
-      
-      return getattr(object.__getattribute__(self, "instance"), attrname)
+      return attribute
 
    def __setattr__(self, attrname, value):
       spyname = object.__getattribute__(self, "spyname")
@@ -27,6 +42,9 @@ if __name__ == "__main__":
   class C:
     def __init__(self):
       self.toto = "3"
+      
+    def foo(self, arg1, arg2):
+      print "in foo: arg1=",arg1," arg2=", arg2
   
   import sys
   a = [1,2,3,3,6,1,-5]
@@ -47,4 +65,7 @@ if __name__ == "__main__":
   
   print "now c.toto has been modified:", c.toto
   
-  #print spyA
+  
+  c.foo(1,3)
+  spyC.foo(1,3)
+  
